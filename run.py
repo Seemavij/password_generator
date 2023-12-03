97d7cb7f692ff66221edf4f6422d22a2c22659fa
Seemavij <>
2023-12-03 09:32:42 +0000

    add password length

--- run.py
+++ run.py
@@ -17,7 +17,14 @@ def get_username():
             print('Invalid username')
     return username
 ​
-​
+​def get_password_length():
+    length_valid = False
+    while length_valid is False:
+        length = input('Enter Password Length: (Max 15)  ')
+        length_valid = validate_input(length, ['4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'])
+        if length_valid is False:
+            print('Invalid length')
+    return length
 
             
 
