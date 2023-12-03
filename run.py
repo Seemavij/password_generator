8cd65669697b085e302871b15336d9e3fde67f29
Seemavij <>
2023-12-03 09:29:56 +0000

    add username

--- run.py
+++ run.py
@@ -7,6 +7,18 @@ def validate_input(user_input, valid_values_list):
         return False
     return user_input.lower() in valid_values_list
 
+def get_username():
+    user_name_valid = False
+    while user_name_valid is False:
+        username = input('Please enter your username:  ')
+        if username:
+            user_name_valid = True
+        else:
+            print('Invalid username')
+    return username
+​
+​
 
+            
 
 
